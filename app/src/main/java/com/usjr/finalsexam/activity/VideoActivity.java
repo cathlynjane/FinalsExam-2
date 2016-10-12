daf0440af961c1baeda9a7fca4a2d7eb3b253721
cathlynjane <cathlynjaneamodia@gmail.com>
2016-10-12 09:35:47 +0800

    Updated VideoActivity

--- app/src/main/java/com/usjr/finalsexam/activity/VideoActivity.java
+++ app/src/main/java/com/usjr/finalsexam/activity/VideoActivity.java
@@ -2,17 +2,22 @@ package com.usjr.finalsexam.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
+
+import com.google.android.youtube.player.YouTubePlayer.Provider;
 import com.usjr.finalsexam.R;
 
-public class VideoActivity extends YouTubeBaseActivity {
+public class VideoActivity extends YouTubeBaseActivity{
 
     private YouTubePlayerView                   mYouTubePlayerView;
     private YouTubePlayer.OnInitializedListener mYoutubeInitializeListener;
+
+    private static final int RECOVERY_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,18 +38,20 @@ public class VideoActivity extends YouTubeBaseActivity {
             @Override
             public void onInitializationSuccess(YouTubePlayer.Provider provider,
                                                 YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.setPlayerStateChangeListener(mYoutubeInitializeListener);
+                youTubePlayer.setPlaybackEventListener(mYoutubeInitializeListener);
                 youTubePlayer.loadVideo("aSdFg123");
             }
 
             @Override
-            public void onInitializationFailure(YouTubePlayer.Provider provider,
+            public void onInitializationFailure(Provider provider,
                                                 YouTubeInitializationResult youTubeInitializationResult) {
-
+                
             }
         };
 
         mYouTubePlayerView.initialize(
-                "AIzaSyBkO-xekoxTzDKdC9jR2KCwT-B1lmFbIlI",
-                mYoutubeInitializeListener);
-    }
+"AIzaSyBkO-xekoxTzDKdC9jR2KCwT-B1lmFbIlI",
+        mYoutubeInitializeListener);
+        }
 }
